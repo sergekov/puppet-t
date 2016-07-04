049cd61b2973ed38f787c38a39a9ec750f69c250
Serge Kovalenko <serg.kovalenko@gmail.com>
2016-07-04 23:05:37 +1000

    Final update

--- project-provision.sh
+++ project-provision.sh
@@ -1,7 +1,7 @@
-vagrant login
+#vagrant login
 #vagrant init centos/7; 
 vagrant up --provider virtualbox
 #Baseline reprovision VM layer if required
 vagrant ssh -c "sudo /etc/puppet/manifests/puppet-t/baseline.sh"
 # Puppet NGINX configuration
-vagrant ssh -c " sudo puppet apply /etc/puppet/manifests/puppet-t/project-configure.pp"
+vagrant ssh -c "sudo puppet apply /etc/puppet/manifests/puppet-t/project-configure.pp"
